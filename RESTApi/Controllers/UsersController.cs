a614a9b66e516ac6ec6435944cb6ab8db209623f
unknown <dansru@gmail.com>
2019-03-18 12:02:21 +0200

    Created usersController and GET-methods

---
+++ RESTApi/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBAccess;
+
+namespace RESTApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IEnumerable<Users> Get()
+        {
+            using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
+            {
+                return entities.Users.ToList();
+            }
+        }
+
+        public Users Get(int ID)
+        {
+            using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
+            {
+                return entities.Users.FirstOrDefault(i => i.id == ID);
+            }
+        }
+
+        public Users Get(string name)
+        {
+            using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
+            {
+                return entities.Users.FirstOrDefault(i => i.username.Equals(name));
+            }
+        }
+    }
+}
