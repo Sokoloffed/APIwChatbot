3b9faf293a1cdf64394065ae3bde5df5b8455a97
unknown <dansru@gmail.com>
2019-03-21 00:00:33 +0200

    Problem with serialization solved

--- RESTApi/Controllers/UsersController.cs
+++ RESTApi/Controllers/UsersController.cs
@@ -5,6 +5,10 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using DBAccess;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Data.Entity;
+
 
 namespace RESTApi.Controllers
 {
@@ -15,6 +19,8 @@ namespace RESTApi.Controllers
         {
             using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
             {
+                //entities.Configuration.LazyLoadingEnabled = false;
+
                 return entities.Users.ToList();
             }
         }
@@ -24,16 +30,36 @@ namespace RESTApi.Controllers
         {
             using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
             {
-                return entities.Users.FirstOrDefault(i => i.id == ID);
+                entities.Configuration.LazyLoadingEnabled = false;
+                Users user = new Users();
+                Users mock = new Users();
+                mock = entities.Users.FirstOrDefault(i => i.id == ID);
+                user.id = mock.id;
+                user.username = mock.username;
+                user.password = mock.password;
+                return user;
             }
         }
 
         [System.Web.Http.HttpGet]
         public Users Get(string name)
         {
+            
             using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
             {
+                
                 return entities.Users.FirstOrDefault(i => i.username.Equals(name));
+            }
+        }
+
+        [Route("api/Users/GetList/{name}")]
+        [HttpGet]
+        public List<Users> GetList(string name)
+        {
+            using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
+            {
+                if (name == "all") return entities.Users.ToList();
+                else return entities.Users.Where(i => i.username.Equals(name)).ToList();
             }
         }
 
@@ -53,27 +79,51 @@ namespace RESTApi.Controllers
 
             }
         }
-        
-        [Route("api/Users/CreateUser/{ID}/name/password")]
+
+        [Route("api/Users/CreateUser")]
         [AcceptVerbs("GET", "POST")]
         [HttpPost]
-        public string CreateUser(int ID, string name, string password)
+        public string CreateUser(Users user)
         {
             using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
             {
-                Users defaultUser = new Users();
-                defaultUser.id = ID;
-                defaultUser.username = name;
-                defaultUser.password = password;
-                if(entities.Users.Where(t => (t.id == ID)).FirstOrDefault() != null)
+
+                if (entities.Users.Where(t => (t.id == user.id)).FirstOrDefault() != null)
                 {
                     return "Attempt to add the existing user.";
                 }
-                entities.Users.Add(defaultUser);
+                entities.Users.Add(user);
                 entities.SaveChanges();
                 return "Success!";
             }
         }
+        
 
+        [Route("api/Users/CreateUser/{ID}")]
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public string CreateUser(int ID)
+        {
+            using (TaskManagerDBEntities entities = new TaskManagerDBEntities())
+            {
+                try
+                {
+                    Users user = new Users();
+                    user.id = ID;
+                    user.username = "testNewPost";
+                    user.password = "testNewPostPassword";
+                    entities.Users.Add(user);
+                    entities.SaveChanges();
+                    return "Entered successfully!";
+
+                }
+                catch
+                {
+                    return "Exception occured!";
+                }
+                
+            }
+        }
     }
+
 }
--- RESTApi/Global.asax.cs
+++ RESTApi/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+
 
 namespace RESTApi
 {
@@ -13,6 +15,8 @@ namespace RESTApi
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
